f001ec6857abc9c02b8210825ad5dc5d4708389d
Valevalefra <valentinayffalas@gmail.com>
2022-03-11 14:17:05 +0100

    change files connection

---
+++ app/utils/connect.go
@@ -0,0 +1,53 @@
+package utils
+
+import (
+	"errors"
+	"log"
+	"os"
+)
+
+type Utils int
+
+// save registration info to reg_node procedure
+func (utils *Utils) Save_registration(arg *Info, res *Result_file) error {
+	log.Printf("The registration is for node whith ip address:port : %s:%s\n", arg.Address, arg.Port)
+	f, err := os.OpenFile("/tmp/clients.txt", os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0755)
+	if err != nil {
+		log.Println(err)
+		return errors.New("Impossible to open file")
+	}
+	/*
+		see https://www.joeshaw.org/dont-defer-close-on-writable-files/ for file defer on close
+	*/
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(f)
+
+	//save new address on file
+	_, err = f.WriteString(arg.Address + ":" + arg.Port)
+	_, err = f.WriteString("\n")
+	err = f.Sync()
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Saved")
+
+	Connection <- true
+	Wg.Add(1)
+	log.Printf("Waiting other connection")
+	Wg.Wait()
+
+	//send back file
+	err = prepare_response(res)
+
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	return nil
+}
--- app/utils/registration_node.go
+++ app/utils/registration_node.go
@@ -18,8 +18,6 @@ const (
 	Server_addr string = "10.10.1.50"
 )
 
-type Utils int
-
 type Result_file struct {
 	PeerNum int
 	Peers   [3]string
@@ -66,50 +64,6 @@ func setInfo(info *Info, port int) error {
 func ParseLine(s string, sep string) (string, string) {
 	res := strings.Split(s, sep)
 	return res[0], res[1]
-}
-
-// save registration info to reg_node procedure
-func (utils *Utils) Save_registration(arg *Info, res *Result_file) error {
-	log.Printf("The registration is for node whith ip address:port : %s:%s\n", arg.Address, arg.Port)
-	f, err := os.OpenFile("/tmp/clients.txt", os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0755)
-	if err != nil {
-		log.Println(err)
-		return errors.New("Impossible to open file")
-	}
-	/*
-		see https://www.joeshaw.org/dont-defer-close-on-writable-files/ for file defer on close
-	*/
-	defer func(f *os.File) {
-		err := f.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}(f)
-
-	//save new address on file
-	_, err = f.WriteString(arg.Address + ":" + arg.Port)
-	_, err = f.WriteString("\n")
-	err = f.Sync()
-	if err != nil {
-		return err
-	}
-
-	log.Printf("Saved")
-
-	Connection <- true
-	Wg.Add(1)
-	log.Printf("Waiting other connection")
-	Wg.Wait()
-
-	//send back file
-	err = prepare_response(res)
-
-	if err != nil {
-		log.Fatal(err)
-		return err
-	}
-
-	return nil
 }
 
 func prepare_response(res *Result_file) error {
@@ -166,7 +120,7 @@ func Registration(peers *list.List, port int) {
 
 	//call procedure
 	log.Printf("Call to registration node")
-	err = server.Call("Save_registration", &info, &res)
+	err = server.Call("Utils.Save_registration", &info, &res)
 	if err != nil {
 		log.Fatal("Error save_registration procedure: ", err)
 	}
