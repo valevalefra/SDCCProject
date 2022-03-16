9ae1688bda512705a368b99087f230dbc3384167
Valevalefra <valentinayffalas@gmail.com>
2022-03-16 17:02:00 +0100

    check file

--- app/node/node.go
+++ app/node/node.go
@@ -3,10 +3,13 @@ package main
 import (
 	"SDCCProject/app/utility"
 	"container/list"
+	"log"
 )
 
 var (
 	peers *list.List
+	myId  int
+	allId []int
 )
 
 func main() {
@@ -14,4 +17,24 @@ func main() {
 	peers = list.New()
 	utility.Registration(peers, 2345)
 
+	for e := peers.Front(); e != nil; e = e.Next() {
+		item := e.Value.(utility.Info)
+		log.Printf("Address: %s:%s", item.Address, item.Port)
+	}
+	//get myId
+	setMyID()
+
+}
+
+func setMyID() {
+
+	for e := peers.Front(); e != nil; e = e.Next() {
+		item := e.Value.(utility.Info)
+		if item.Address == utility.GetLocalIP() {
+			myId = item.ID
+			allId = append(allId, item.ID)
+		} else {
+			allId = append(allId, item.ID)
+		}
+	}
 }
