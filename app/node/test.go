a654bcac3db2bb951a7377510499f007c2e621db
Valevalefra <valentinayffalas@gmail.com>
2022-03-31 22:25:42 +0200

    update test

--- app/node/test.go
+++ app/node/test.go
@@ -35,7 +35,7 @@ func executeTest(id int, test func(testId int)) {
 */
 func testLamport(testId int) {
 	const numMsg = 3 * utility.MAXPEERS //3 msg per peer
-	msgs := [3]string{"a", "b", "c"}
+	msgs := [3]string{"a", "b"}
 
 	algorithmChoosen = 0
 	for _, s := range msgs {
