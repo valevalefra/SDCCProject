3fd56827613f8ab64c040fa1432150636482ee84
Valevalefra <valentinayffalas@gmail.com>
2022-04-01 15:59:20 +0200

    update test

--- app/node/node_sender.go
+++ app/node/node_sender.go
@@ -53,7 +53,7 @@ func send_to_peer(msg utility.Message, senderId int) {
 	if senderId == -1 {
 		fmt.Printf("sono prima del for di send to peer \n")
 		for e := peers.Front(); e != nil; e = e.Next() {
-			listNode[0].numberOfMessage = +1
+			listNode[0].numberOfMessage = listNode[0].numberOfMessage + 1
 			dest := e.Value.(utility.Info)
 			fmt.Printf("sono dopo il for di send to peer e mando a %d \n", dest)
 			//open connection whit other peer
@@ -78,7 +78,7 @@ func send_to_peer(msg utility.Message, senderId int) {
 	//send to other peer excluded me
 	if senderId == -2 {
 		for e := peers.Front(); e != nil; e = e.Next() {
-			listNode[0].numberOfMessage = +1
+			listNode[0].numberOfMessage = listNode[0].numberOfMessage + 1
 			if e.Value.(utility.Info).ID != msg.SendID {
 				dest := e.Value.(utility.Info)
 				log.Printf("sto per mandare il mess di release al nodo con id %d sono il nodo con id %d \n", e.Value.(utility.Info).ID, msg.SendID)
@@ -124,7 +124,7 @@ func send_reply(id int, text string) {
 	msg.Type = 2
 	msg.Text = text
 	msg.SendID = myId
-	listNode[0].numberOfMessage = +1
+	listNode[0].numberOfMessage = listNode[0].numberOfMessage + 1
 	//send to specific peer
 	for e := peers.Front(); e != nil; e = e.Next() {
 		if e.Value.(utility.Info).ID == id {
--- app/node/test.go
+++ app/node/test.go
@@ -37,7 +37,7 @@ func executeTest(id int, test func(testId int)) {
 */
 func testLamport(testId int) {
 	const numMsg = 3 * utility.MAXPEERS //3 msg per peer
-	msgs := [1]string{"abc" + strconv.Itoa(myId)}
+	msgs := [1]string{"ciao sono il nodo " + strconv.Itoa(myId)}
 
 	algorithmChoosen = 0
 	for _, s := range msgs {
