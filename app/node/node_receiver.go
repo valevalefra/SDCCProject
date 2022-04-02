4614344075ea357cc1427ae2dcbfe8f0a9fe529f
Valevalefra <valentinayffalas@gmail.com>
2022-04-02 16:02:28 +0200

    clean files

--- app/node/node_receiver.go
+++ app/node/node_receiver.go
@@ -100,13 +100,13 @@ func handleConnection(connection net.Conn) {
 
 	case utility.Reply:
 
-		fmt.Printf("il nodo con id %d ha ricevuto un mess di reply, per il mess %s, dal nodo con id %d che ha valore del clock pari a %d \n", myId, msg.Text, msg.SendID, msg.Clock[0])
+		fmt.Printf("il nodo con id %d ha ricevuto un mess di reply, per il mess %s, dal nodo con id %d che ha valore del clock pari a %d \n", myId, msg.Text, msg.SendID, msg.Clock)
 		text := msg.Text
 		ackChan <- text
 
 	case utility.Release:
 		if algorithmChoosen == 0 {
-			fmt.Printf("il nodo con id %d ha ricevuto un mess di release, per il mess %s, dal nodo con id %d che ha valore del clock pari a %d \n", myId, msg.Text, msg.SendID, msg.Clock[0])
+			fmt.Printf("il nodo con id %d ha ricevuto un mess di release, per il mess %s, dal nodo con id %d che ha valore del clock pari a %d \n", myId, msg.Text, msg.SendID, msg.Clock)
 			//delete msg from queue
 			l := scalarMsgQueue
 			if l.Len() != 0 {
